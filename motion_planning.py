bb669d12af17e7d405d69be236038be272eb7bd5
OSX <osx@iMac.local>
2018-04-12 23:47:22 +0900

    finito

--- motion_planning.py
+++ motion_planning.py
@@ -11,6 +11,29 @@ from udacidrone.connection import MavlinkConnection
 from udacidrone.messaging import MsgID
 from udacidrone.frame_utils import global_to_local
 
+def collinearity_2D(p1, p2, p3):
+    collinear = False
+    det = p1[0]*(p2[1] - p3[1]) + p2[0]*(p3[1] - p1[1]) + p3[0]*(p1[1] - p2[1])
+    if det == 0 :
+        collinear = True
+
+    return collinear
+
+def prune_path(path):
+    pruned_path = [p for p in path]
+
+    i = 0
+    while i < len(pruned_path) - 2:
+        p1 = pruned_path[i]
+        p2 = pruned_path[i+1]
+        p3 = pruned_path[i+2]
+
+        if collinearity_2D(p1, p2, p3):
+            pruned_path.remove(pruned_path[i+1])
+        else:
+            i += 1
+    return pruned_path
+
 
 class States(Enum):
     MANUAL = auto()
@@ -115,41 +138,56 @@ class MotionPlanning(Drone):
         self.flight_state = States.PLANNING
         print("Searching for a path ...")
         TARGET_ALTITUDE = 5
-        SAFETY_DISTANCE = 5
+        SAFETY_DISTANCE = 8
 
         self.target_position[2] = TARGET_ALTITUDE
 
         # TODO: read lat0, lon0 from colliders into floating point values
-        
+        with open('colliders.csv') as f:
+            home_pos_data = f.readline().split(",")
+        lat0 = float(home_pos_data[0].strip().split(" ")[1])
+        lon0 = float(home_pos_data[1].strip().split(" ")[1])
+        print(lat0, lon0)
+
         # TODO: set home position to (lon0, lat0, 0)
+        self.set_home_position(lon0, lat0, 0)
 
         # TODO: retrieve current global position
- 
+        global_pos = [self._longitude, self._latitude, self._altitude]
+
         # TODO: convert to current local position using global_to_local()
-        
+        local_pos = global_to_local(global_pos, self.global_home)
+
         print('global home {0}, position {1}, local position {2}'.format(self.global_home, self.global_position,
                                                                          self.local_position))
         # Read in obstacle map
         data = np.loadtxt('colliders.csv', delimiter=',', dtype='Float64', skiprows=2)
-        
+
         # Define a grid for a particular altitude and safety margin around obstacles
         grid, north_offset, east_offset = create_grid(data, TARGET_ALTITUDE, SAFETY_DISTANCE)
         print("North offset = {0}, east offset = {1}".format(north_offset, east_offset))
         # Define starting point on the grid (this is just grid center)
         grid_start = (-north_offset, -east_offset)
         # TODO: convert start position to current position rather than map center
-        
+        grid_start = (int(local_pos[0]-north_offset), int(local_pos[1]- east_offset))
+
         # Set goal as some arbitrary position on the grid
         grid_goal = (-north_offset + 10, -east_offset + 10)
         # TODO: adapt to set goal as latitude / longitude position and convert
+        # picked location from simulator manually controlled lat=37.794752, lon = -122.392150
+        goal_pos = global_to_local([-122.400886, 37.795174, self._altitude], self.global_home)
+        grid_goal = (int(goal_pos[0]-north_offset), int(goal_pos[1]-east_offset))
+
 
         # Run A* to find a path from start to goal
         # TODO: add diagonal motions with a cost of sqrt(2) to your A* implementation
         # or move to a different search space such as a graph (not done here)
         print('Local Start and Goal: ', grid_start, grid_goal)
         path, _ = a_star(grid, heuristic, grid_start, grid_goal)
+
         # TODO: prune path to minimize number of waypoints
         # TODO (if you're feeling ambitious): Try a different approach altogether!
+        path = prune_path(path)
 
         # Convert path to waypoints
         waypoints = [[p[0] + north_offset, p[1] + east_offset, TARGET_ALTITUDE, 0] for p in path]
@@ -157,6 +195,7 @@ class MotionPlanning(Drone):
         self.waypoints = waypoints
         # TODO: send waypoints to sim (this is just for visualization of waypoints)
         self.send_waypoints()
+
 
     def start(self):
         self.start_log("Logs", "NavLog.txt")
